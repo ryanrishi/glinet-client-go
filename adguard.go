20500bd3b185ef7003de7a5745bb2acdfe10baa8
Ryan Rishi <ryan@ryanrishi.com>
2023-02-22 19:11:59 -0500

    Add AdGuard (#2)

---
+++ adguard.go
@@ -0,0 +1,44 @@
+package glinet
+
+type AdGuardService service
+
+type GetAdGuardConfigResponse struct {
+	Enabled bool `json:"enabled"`
+}
+
+type SetAdGuardConfigRequest struct {
+	Enabled bool `json:"enabled"`
+}
+
+func (s *AdGuardService) GetAdGuardConfig() (*GetAdGuardConfigResponse, error) {
+	var res GetAdGuardConfigResponse
+
+	err := s.client.CallWithStringSlice("call", []string{s.client.Sid, "adguardhome", "get_config"}, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+func (s *AdGuardService) SetAdGuardConfig(Enabled bool) error {
+	var req = SetAdGuardConfigRequest{
+		Enabled,
+	}
+
+	var params = make([]interface{}, 4)
+	params[0] = s.client.Sid
+	params[1] = "adguardhome"
+	params[2] = "set_config"
+	params[3] = &req
+
+	// don't care about response; it's `[]` if successful
+	var res []byte
+
+	err := s.client.CallWithInterfaceSlice("call", params, &res)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
